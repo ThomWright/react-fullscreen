6cd6abe8fb3d59495f9adafff811cfe0d54872ca
ThomWright <github@thomwright.co.uk>
2015-07-17 14:51:15 +0100

    Add implementation

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react/addons';
+
+var getDimensions = () => ({width: window.innerWidth, height: window.innerHeight});
+
+export default class FullScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = getDimensions();
+
+    this.handleResize = this.handleResize.bind(this);
+  }
+
+  handleResize() {
+    this.setState(getDimensions());
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  render() {
+    const child = React.addons.cloneWithProps(React.Children.only(this.props.children),
+      {
+        width: this.state.width,
+        height: this.state.height
+      }
+    );
+    return child;
+  }
+
+}
